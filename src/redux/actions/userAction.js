d7ba96206c7daf74764ac0c696e062d66144be38
iMac <imac@iMacs-iMac.local>
2022-03-31 17:24:21 +0500

    code-fixes

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,31 +1,22 @@
 import ApiService from "../../services/api";
-import {
-  createUserError,
-  createUserStarting,
-  createUserSuccess,
-  updateUserError,
-  updateUserStarting,
-  updateUserSuccess,
-  userError,
-  userStarting,
-  userSuccess,
-} from "./typeFunctions";
+import * as userTypes from "../actionTypes/userActionTypes";
+import { handleType } from "./handleType";
 
 export const getUsers = (payload, token) => (dispatch) => {
-  dispatch(userStarting());
+  dispatch(handleType(userTypes.USER_START));
   const { search, limit, offset, organization } = payload;
   return ApiService.getResources(
     `/v1/users/?organization=${organization}&search=${search}&limit=${limit}&offset=${offset}`,
     token
   )
     .then((value) => {
-      dispatch(userSuccess(value));
+      dispatch(handleType(userTypes.USER_SUCCESS, value));
     })
-    .catch((error) => dispatch(userError(error)));
+    .catch((error) => dispatch(handleType(userTypes.USER_FAIL, error)));
 };
 
 export const createUser = (payload, token) => (dispatch) => {
-  dispatch(createUserStarting());
+  dispatch(handleType(userTypes.CREATE_USER_START));
   const { login_name, password, role, organization } = payload;
   const body = {
     login_name,
@@ -35,13 +26,13 @@ export const createUser = (payload, token) => (dispatch) => {
   };
   return ApiService.fetchPostEvent("/v1/users/", token, body)
     .then((value) => {
-      dispatch(createUserSuccess(value));
+      dispatch(handleType(userTypes.CREATE_USER_SUCCESS, value));
     })
-    .catch((error) => dispatch(createUserError(error)));
+    .catch((error) => dispatch(handleType(userTypes.CREATE_USER_FAIL, error)));
 };
 
 export const updateUser = (id, payload, token) => (dispatch) => {
-  dispatch(updateUserStarting());
+  dispatch(handleType(userTypes.UPDATE_USER_START));
   const { login_name, password } = payload;
   const body = {
     login_name,
@@ -49,7 +40,7 @@ export const updateUser = (id, payload, token) => (dispatch) => {
   };
   return ApiService.patchEvent(`/v1/users/${id}/`, token, body)
     .then((value) => {
-      dispatch(updateUserSuccess(value));
+      dispatch(handleType(userTypes.UPDATE_USER_SUCCESS, value));
     })
-    .catch((error) => dispatch(updateUserError(error)));
+    .catch((error) => dispatch(handleType(userTypes.UPDATE_USER_FAIL, error)));
 };
